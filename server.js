709f4bd87c0314903106d4064a0c035cf6888650
Hyrum Perez <hyrumpa@gmail.com>
2024-09-21 20:30:20 -0500

    Advances

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
+
 connectDB();
 
-// Define routes
+
 app.get('/', getAllContacts);
 app.get('/:id', getContactById);
 app.post('/', createContact);
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
